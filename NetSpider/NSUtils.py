b74142e3c3b2317253712dee49f8d406df9d74df
huangxiaobo <huangxb7818@gmail.com>
2015-08-18 23:27:12 +0800

    增加一个调试的Decorator
    增加一个调试的Decorator

--- NetSpider/NSUtils.py
+++ NetSpider/NSUtils.py
@@ -1,4 +1,4 @@
-#encoding=utf8
+# -*- encoding=utf8 -*-
 HEADER = '\033[95m'
 OKBLUE = '\033[94m'
 OKGREEN = '\033[92m'
@@ -7,13 +7,15 @@ FAIL = '\033[91m'
 ENDC = '\033[0m'
 BOLD = "\033[1m"
 
+
 def disable():
-		HEADER = ''
-		OKBLUE = ''
-		OKGREEN = ''
-		WARNING = ''
-		FAIL = ''
-		ENDC = ''
+	HEADER = ''
+	OKBLUE = ''
+	OKGREEN = ''
+	WARNING = ''
+	FAIL = ''
+	ENDC = ''
+
 
 def infog( msg):
 	print OKGREEN + msg + ENDC
@@ -26,3 +28,29 @@ def warn( msg):
 
 def err( msg):
 	print FAIL + msg + ENDC
+
+__report_indent = [0]
+def debug(fn):
+	"""Decorator to print information about a function call for use while debugging.
+	Prints function name, arguments, and call number when the function is called. Prints this information
+	again along with the return value when the function returns.
+	"""
+
+	def wrap(*params,**kwargs):
+		call = wrap.callcount = wrap.callcount + 1
+
+		indent = ' ' * __report_indent[0]
+		fc = "%s(%s)" % (fn.__name__, ', '.join(
+			[a.__repr__() for a in params[1:]] +
+			["%s = %s" % (a, repr(b)) for a,b in kwargs.items()]
+		))
+
+		print "%s%s called [#%s]" % (indent, fc, call)
+		__report_indent[0] += 1
+		ret = fn(*params,**kwargs)
+		__report_indent[0] -= 1
+		print "%s%s returned %s [#%s]" % (indent, fc, repr(ret), call)
+
+		return ret
+	wrap.callcount = 0
+	return wrap
